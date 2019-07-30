fa78759be0e0fa513791d22c400c00750d87876d
Max Walters <max.walters10@gmail.com>
2019-07-30 12:39:41 -0400

    Working on OWM API

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
 import '../location.dart';
+import 'dart:convert';
+import '../apiKey.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -8,6 +11,9 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  
+  double latitude;
+  double longitude;
   
   @override
   void initState() {
@@ -18,7 +24,24 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void getLocation() async {
     Location locationObj = new Location();
     await locationObj.getCurrentLocation();
-    print(locationObj.latitude.toString() + ", " + locationObj.longitude.toString());
+    
+    latitude = locationObj.latitude;
+    longitude = locationObj.longitude;
+
+    getData();
+  }
+
+  void getData() async {
+    http.Response response = await http.get('http://api.openweathermap.org/data/2.5/weather?lat=$latitude&$longitude&appid=$apiKey');
+    
+    if(response.statusCode == 200) {
+      String data = response.body;
+      String name = jsonDecode(data)['name'];
+      double temp = jsonDecode(data)['main']['temp'];
+      int cond = jsonDecode(data)['weather'][0]['id'];
+    } else {
+      print(response.statusCode);
+    }
   }
   
   @override
